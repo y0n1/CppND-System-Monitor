d5af2375d9e8c2ef0338746665950f4eea5a73c8
Jonathan Kilzi <jonathan.kilzi@888holdings.com>
2019-12-19 17:27:39 +0000

    refactor: Implements Format::ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,19 @@
+#include <cstdio>
 #include <string>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long totalSeconds) {
+  int seconds = totalSeconds % 60;
+  int minutes = (totalSeconds / 60) % 60;
+  int hours = (totalSeconds / 3600) % 24;
+
+  const string OUTPUT_FORMAT {"HH:MM:SS"};
+  char buffer[OUTPUT_FORMAT.length + 1];
+  sprintf(buffer, "%02d:%02d:%02d", hours, minutes, seconds);
+  string uptime {buffer};
+
+  return uptime;
+}
